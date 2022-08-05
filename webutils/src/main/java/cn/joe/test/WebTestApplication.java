26e11b07e4c586b7cd6ec7084057b6a23e8b11ed
lijian <lijian@cmss.chinamobile.com>
2022-08-05 11:12:53 +0800

    [project4utils] 获取Spring容器上下文
    Code Source From: Self Code
    Description: [Optional]
    Jira:
    市场项目编号（名称）：

--- webutils/src/main/java/cn/joe/test/WebTestApplication.java
+++ webutils/src/main/java/cn/joe/test/WebTestApplication.java
@@ -2,11 +2,13 @@ package cn.joe.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebTestApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(WebTestApplication.class , args);
+    ConfigurableApplicationContext context =  SpringApplication.run(WebTestApplication.class , args);
+    Object sqlSessionTemplate = context.getBean("sqlSessionTemplate");
   }
 }
